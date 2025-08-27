7a01d1a8a88ece66707d0ede47bc1d1af8b685c7
Gabriel Oliveira <bielgabrieloliveira77@gmail.com>
2025-08-26 23:12:21 -0300

    Atualiza interface do usuário e renomeia componentes
    Modifica nomes e funcionalidades de diversos elementos da interface, incluindo botões e caixas de texto. Adiciona novos botões para operações de inclusão, alteração e listagem. Renomeia botões de radio para refletir técnicas de hashing. Substitui `DataGridView` por `lsbListagem` para exibição de dados.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,36 +30,36 @@
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radio_BucketHash = new System.Windows.Forms.RadioButton();
+            this.radio_Linear = new System.Windows.Forms.RadioButton();
+            this.radio_Quadratica = new System.Windows.Forms.RadioButton();
+            this.radio_duploHash = new System.Windows.Forms.RadioButton();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_palavra = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_Dica = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Excluir = new System.Windows.Forms.Button();
             this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Alterar = new System.Windows.Forms.Button();
+            this.btn_Incluir = new System.Windows.Forms.Button();
+            this.btn_Listar = new System.Windows.Forms.Button();
+            this.lsbListagem = new System.Windows.Forms.DataGridView();
             this.flowLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel2.SuspendLayout();
             this.flowLayoutPanel3.SuspendLayout();
             this.flowLayoutPanel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lsbListagem)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.label1);
-            this.flowLayoutPanel1.Controls.Add(this.radioButton1);
-            this.flowLayoutPanel1.Controls.Add(this.radioButton2);
-            this.flowLayoutPanel1.Controls.Add(this.radioButton3);
-            this.flowLayoutPanel1.Controls.Add(this.radioButton4);
+            this.flowLayoutPanel1.Controls.Add(this.radio_BucketHash);
+            this.flowLayoutPanel1.Controls.Add(this.radio_Linear);
+            this.flowLayoutPanel1.Controls.Add(this.radio_Quadratica);
+            this.flowLayoutPanel1.Controls.Add(this.radio_duploHash);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(135, 115);
@@ -74,54 +74,54 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Técnicas de Hashing";
             // 
-            // radioButton1
+            // radio_BucketHash
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(3, 16);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(87, 17);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Bucket Hash";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radio_BucketHash.AutoSize = true;
+            this.radio_BucketHash.Location = new System.Drawing.Point(3, 16);
+            this.radio_BucketHash.Name = "radio_BucketHash";
+            this.radio_BucketHash.Size = new System.Drawing.Size(87, 17);
+            this.radio_BucketHash.TabIndex = 1;
+            this.radio_BucketHash.TabStop = true;
+            this.radio_BucketHash.Text = "Bucket Hash";
+            this.radio_BucketHash.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // radio_Linear
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(3, 39);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(108, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Sondagem Linear";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radio_Linear.AutoSize = true;
+            this.radio_Linear.Location = new System.Drawing.Point(3, 39);
+            this.radio_Linear.Name = "radio_Linear";
+            this.radio_Linear.Size = new System.Drawing.Size(108, 17);
+            this.radio_Linear.TabIndex = 2;
+            this.radio_Linear.TabStop = true;
+            this.radio_Linear.Text = "Sondagem Linear";
+            this.radio_Linear.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // radio_Quadratica
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(3, 62);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(131, 17);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Sondagem Quadrática";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radio_Quadratica.AutoSize = true;
+            this.radio_Quadratica.Location = new System.Drawing.Point(3, 62);
+            this.radio_Quadratica.Name = "radio_Quadratica";
+            this.radio_Quadratica.Size = new System.Drawing.Size(131, 17);
+            this.radio_Quadratica.TabIndex = 3;
+            this.radio_Quadratica.TabStop = true;
+            this.radio_Quadratica.Text = "Sondagem Quadrática";
+            this.radio_Quadratica.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // radio_duploHash
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(3, 85);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(81, 17);
-            this.radioButton4.TabIndex = 4;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Duplo Hash";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radio_duploHash.AutoSize = true;
+            this.radio_duploHash.Location = new System.Drawing.Point(3, 85);
+            this.radio_duploHash.Name = "radio_duploHash";
+            this.radio_duploHash.Size = new System.Drawing.Size(81, 17);
+            this.radio_duploHash.TabIndex = 4;
+            this.radio_duploHash.TabStop = true;
+            this.radio_duploHash.Text = "Duplo Hash";
+            this.radio_duploHash.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.Controls.Add(this.label2);
-            this.flowLayoutPanel2.Controls.Add(this.textBox1);
+            this.flowLayoutPanel2.Controls.Add(this.textBox_palavra);
             this.flowLayoutPanel2.Location = new System.Drawing.Point(173, 14);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(480, 31);
@@ -136,12 +136,12 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Palavra:";
             // 
-            // textBox1
+            // textBox_palavra
             // 
-            this.textBox1.Location = new System.Drawing.Point(55, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(411, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBox_palavra.Location = new System.Drawing.Point(55, 3);
+            this.textBox_palavra.Name = "textBox_palavra";
+            this.textBox_palavra.Size = new System.Drawing.Size(411, 20);
+            this.textBox_palavra.TabIndex = 1;
             // 
             // label3
             // 
@@ -152,83 +152,83 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Dica:";
             // 
-            // textBox2
+            // textBox_Dica
             // 
-            this.textBox2.Location = new System.Drawing.Point(41, 3);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(425, 20);
-            this.textBox2.TabIndex = 1;
+            this.textBox_Dica.Location = new System.Drawing.Point(41, 3);
+            this.textBox_Dica.Name = "textBox_Dica";
+            this.textBox_Dica.Size = new System.Drawing.Size(425, 20);
+            this.textBox_Dica.TabIndex = 1;
             // 
             // flowLayoutPanel3
             // 
             this.flowLayoutPanel3.Controls.Add(this.label3);
-            this.flowLayoutPanel3.Controls.Add(this.textBox2);
+            this.flowLayoutPanel3.Controls.Add(this.textBox_Dica);
             this.flowLayoutPanel3.Location = new System.Drawing.Point(173, 51);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
             this.flowLayoutPanel3.Size = new System.Drawing.Size(480, 31);
             this.flowLayoutPanel3.TabIndex = 2;
             // 
-            // button1
+            // btn_Excluir
             // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Excluir";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_Excluir.Location = new System.Drawing.Point(3, 3);
+            this.btn_Excluir.Name = "btn_Excluir";
+            this.btn_Excluir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Excluir.TabIndex = 3;
+            this.btn_Excluir.Text = "Excluir";
+            this.btn_Excluir.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel4
             // 
-            this.flowLayoutPanel4.Controls.Add(this.button1);
-            this.flowLayoutPanel4.Controls.Add(this.button2);
-            this.flowLayoutPanel4.Controls.Add(this.button3);
-            this.flowLayoutPanel4.Controls.Add(this.button4);
+            this.flowLayoutPanel4.Controls.Add(this.btn_Excluir);
+            this.flowLayoutPanel4.Controls.Add(this.btn_Alterar);
+            this.flowLayoutPanel4.Controls.Add(this.btn_Incluir);
+            this.flowLayoutPanel4.Controls.Add(this.btn_Listar);
             this.flowLayoutPanel4.Location = new System.Drawing.Point(173, 97);
             this.flowLayoutPanel4.Name = "flowLayoutPanel4";
             this.flowLayoutPanel4.Size = new System.Drawing.Size(328, 34);
             this.flowLayoutPanel4.TabIndex = 4;
             // 
-            // button2
+            // btn_Alterar
             // 
-            this.button2.Location = new System.Drawing.Point(84, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Alterar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_Alterar.Location = new System.Drawing.Point(84, 3);
+            this.btn_Alterar.Name = "btn_Alterar";
+            this.btn_Alterar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Alterar.TabIndex = 4;
+            this.btn_Alterar.Text = "Alterar";
+            this.btn_Alterar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btn_Incluir
             // 
-            this.button3.Location = new System.Drawing.Point(165, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Incluir";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btn_Incluir.Location = new System.Drawing.Point(165, 3);
+            this.btn_Incluir.Name = "btn_Incluir";
+            this.btn_Incluir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Incluir.TabIndex = 5;
+            this.btn_Incluir.Text = "Incluir";
+            this.btn_Incluir.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btn_Listar
             // 
-            this.button4.Location = new System.Drawing.Point(246, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "Listar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btn_Listar.Location = new System.Drawing.Point(246, 3);
+            this.btn_Listar.Name = "btn_Listar";
+            this.btn_Listar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Listar.TabIndex = 6;
+            this.btn_Listar.Text = "Listar";
+            this.btn_Listar.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // lsbListagem
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 147);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(635, 207);
-            this.dataGridView1.TabIndex = 5;
+            this.lsbListagem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lsbListagem.Location = new System.Drawing.Point(18, 147);
+            this.lsbListagem.Name = "lsbListagem";
+            this.lsbListagem.Size = new System.Drawing.Size(635, 207);
+            this.lsbListagem.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(671, 376);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lsbListagem);
             this.Controls.Add(this.flowLayoutPanel4);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -242,7 +242,7 @@
             this.flowLayoutPanel3.ResumeLayout(false);
             this.flowLayoutPanel3.PerformLayout();
             this.flowLayoutPanel4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lsbListagem)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -251,22 +251,22 @@
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radio_BucketHash;
+        private System.Windows.Forms.RadioButton radio_Linear;
+        private System.Windows.Forms.RadioButton radio_Quadratica;
+        private System.Windows.Forms.RadioButton radio_duploHash;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox_palavra;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_Dica;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Excluir;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Alterar;
+        private System.Windows.Forms.Button btn_Incluir;
+        private System.Windows.Forms.Button btn_Listar;
+        private System.Windows.Forms.DataGridView lsbListagem;
     }
 }
 
